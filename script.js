af1e07f51996052d30cea7b64f3a17d0ebb2cd6d
Mostafa atef <146730066+Mostafa21atef@users.noreply.github.com>
2023-11-06 16:07:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// // JavaScript to handle form validation
+// function validateForm() {
+//     var fullname = document.getElementById('fullname').value;
+//     var email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
+//     var confirmPassword = document.getElementById('confirm-password').value;
+
+//     if (password !== confirmPassword) {
+//         alert("Passwords do not match. Please try again.");
+//         return false;
+//     }
+
+//     // You can add more validation rules here, such as email format validation.
+
+//     // If all validation passes, you can submit the form to your server.
+//     // In a real-world scenario, this is where you'd make an AJAX request to your server to handle registration.
+
+//     return true;
+// }
